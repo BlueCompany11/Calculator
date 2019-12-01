a1ec9741928f42ee33d916204106df125c8a96fb
BlueCompany11 <gronczewskim@gmail.com>
2019-12-01 10:57:36 +0100

    Created layout

---
+++ ExceptionalCalculator/EquasionCount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionalCalculator
+{
+    public class EquasionCount
+    {
+        string equasion;
+        public EquasionCount()
+        {
+
+        }
+
+        public void InsertEquasion(string equasion)
+        {
+            this.equasion = equasion;
+        }
+
+
+        public double Count()
+        {
+            double val = 0;
+
+            return val;
+        }
+    }
+}
